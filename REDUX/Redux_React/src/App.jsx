8e1b8952af3f9dc983adb7aa98a0796c1e51feba
Sagar Gautam <sagargautam389@gmail.com>
2025-02-20 13:49:58 +0545

    bootstrap

--- REDUX/Redux_React/src/App.jsx
+++ REDUX/Redux_React/src/App.jsx
@@ -2,9 +2,13 @@ import './App.css'
 
 function App() {
 
+  console.log("react-redux......................!")
   return (
     <>
       <h1>React_Redux:</h1>
+      
+      <button type="button" className="btn btn-success">Success</button>
+      
     </>
   )
 }
