65bef8d1a7181591553f5c7183a130a217fac543
Sagar Gautam <sagargautam389@gmail.com>
2025-02-21 09:34:36 +0545

    reduxOnly

--- REDUX/1ReduxOnly/ReduxDemo.js
+++ REDUX/1ReduxOnly/ReduxDemo.js
@@ -1,1 +1,28 @@
-console.log("redux demo....");
+const redux = require('redux');
+
+//reducer function declare::
+let INITIAL_STATE = { Counter: 0 };
+
+const reducer = (state = INITIAL_STATE, action) => {
+  if (action.type === "INCREMENT") {
+    return { Counter: state.Counter + 1 };
+  }
+  if (action.type === "DECREMENT") {
+    return { Counter: state.Counter - 1 };
+  }
+  // Return the current state for unknown actions
+  return state;
+};
+
+//create store:::
+const store = redux.createStore(reducer);
+
+
+//Subscribe() Method::: gain new state through callback function()
+store.subscribe(() => {
+  console.log('State Updated:', store.getState());
+});
+
+//Dispatch() method::: send action{}
+// store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
